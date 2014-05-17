6acba6e8c85ae9aacc53e4bad63c531cd1150287
Yigit Ozen <canyigitozen@gmail.com>
2014-05-17 15:38:11 +0300

    Fix Templates

--- buscompany/views/main.py
+++ buscompany/views/main.py
@@ -4,8 +4,7 @@ from django.http import HttpResponseRedirect
 from django.db import connection
 
 def index(request):
-	lookupForm = VoyageLookupForm()
-	return render(request, 'main.html',{'lookupForm':lookupForm})
+	return render(request, 'frontpage.html')
 
 
 # an incomplete half-dummy implementation of listVoyages just to see how forms work
@@ -19,18 +18,18 @@ def listVoyages(request):
 			cursor.execute('''SELECT r.route_id From Route r,Terminal t1,Terminal t2
 							WHERE r.depart_terminal=t1.id AND t1.city=%s AND r.arrive_terminal=t2.id AND t2.city=%s''',
 							[form.cleaned_data['departure_city'],form.cleaned_data['arrival_city']])
-			
+
 			route_id = cursor.fetchone()
 			if route_id is not None:
 
 				#date hardcoded in sql statement. FIX THIS!!!
 				cursor.execute('''SELECT v.departure_time,v.arrival_time,bt.model,v.occupied_seats,b.plate FROM Voyage v,Bus b,BusType bt
-								 WHERE v.route_id=%s AND b.plate = v.plate AND bt.id=b.bustype_id 
+								 WHERE v.route_id=%s AND b.plate = v.plate AND bt.id=b.bustype_id
 								 AND v.departure_time >= %s AND v.departure_time< "2014-06-02"''',
 								 [route_id[0],form.cleaned_data['date']])
 
 				rows=cursor.fetchall()
 	else:
 		form = VoyageLookupForm()
-	return render(request,'listVoyages.html',{'lookupForm':form,'rows':rows})
+	return render(request,'list_voyages.html',{'lookupForm':form,'rows':rows})
 
